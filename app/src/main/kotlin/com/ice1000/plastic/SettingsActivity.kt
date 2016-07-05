56cf8099c7e1c1b41a1329d0e1554846111786b6
ice1000 <ice1000kotlin@foxmail.com>
2016-07-05 13:04:25 +0800

    fix data parsing view

--- app/src/main/kotlin/com/ice1000/plastic/SettingsActivity.kt
+++ app/src/main/kotlin/com/ice1000/plastic/SettingsActivity.kt
@@ -1,9 +1,13 @@
 package com.ice1000.plastic
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import data.JJFLY
+import kotlinx.android.synthetic.main.activity_scrolling.*
 
 import kotlinx.android.synthetic.main.activity_settings.*
+import org.jetbrains.anko.toast
 import utils.BaseActivity
 import utils.indexLink
 class SettingsActivity : BaseActivity() {
@@ -14,15 +18,36 @@ class SettingsActivity : BaseActivity() {
         setContentView(R.layout.activity_settings)
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        insert_test_data.setOnClickListener {
-            insertIntoSp(indexLink, JJFLY)
+        val fab = fab
+        fab.setOnClickListener({
+            joinQQGroup("cAyOYEJxVW0zF4mR5XNZ1uoFk5qEBMlR")
+        })
+
+        viewGiuHub.setOnClickListener {
+            viewGitHub()
         }
 
-        delete_test_data.setOnClickListener {
-            insertIntoSp(
-                    indexLink,
-                    DEFAULT_VALUE
-            )
+    }
+
+    fun viewGitHub() = openWeb("https://github.com/ice1000/PlasticApp")
+
+    /****************
+     * @param key 由官网生成的key
+     * @return 返回true表示呼起手Q成功，返回false表示呼起失败
+     */
+    fun joinQQGroup(key: String): Boolean {
+        val intent = Intent()
+        intent.data = Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com" +
+                "%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key)
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，
+        // 返回手Q主界面，不设置，按返回会返回到呼起产品界面
+        // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            startActivity(intent)
+            return true
+        } catch (e: Exception) {
+            toast(getString(R.string.please_install_qq))
+            return false
         }
     }
 }
