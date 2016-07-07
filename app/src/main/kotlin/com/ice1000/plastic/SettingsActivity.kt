8a6b9236c99be69a6485a929e79c6b6a4ccfd0a6
ice1000 <ice1000kotlin@foxmail.com>
2016-07-07 20:36:39 +0800

    finished....I wonder if this will work.

--- app/src/main/kotlin/com/ice1000/plastic/SettingsActivity.kt
+++ app/src/main/kotlin/com/ice1000/plastic/SettingsActivity.kt
@@ -1,12 +1,16 @@
 package com.ice1000.plastic
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import data.constants.LAYOUT_LIST
+import data.constants.LAYOUT_PREFERENCE
+import kotlinx.android.synthetic.main.activity_settings.*
+import utils.BaseActivity
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +18,40 @@ class SettingsActivity : AppCompatActivity() {
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        initViews()
     }
 
+    fun initViews() {
+        val adapter = ArrayAdapter.createFromResource(
+                this@SettingsActivity,
+                R.array.spinner_names,
+                android.R.layout.simple_spinner_item
+        )
+        val spinner = spinner_settings_layout
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = SpinnerSelected()
+
+    }
+
+    inner class SpinnerSelected() : AdapterView.OnItemSelectedListener {
+        override fun onNothingSelected(p0: AdapterView<*>?) {
+            insertIntoSharedPreference(
+                    key = LAYOUT_PREFERENCE,
+                    value = LAYOUT_LIST
+            )
+        }
+
+        /**
+         * @param p2 is the item which is selected.
+         * p2 -> LAYOUT_* in data.constants.DefaultValue
+         */
+        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            insertIntoSharedPreference(
+                    key = LAYOUT_PREFERENCE,
+                    value = p2
+            )
+        }
+    }
 }
