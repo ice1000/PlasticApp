19aaefa58f5d068dd50156523bc753e006b020b3
ice1000 <ice1000kotlin@foxmail.com>
2016-07-11 13:55:50 +0800

    add textSize setter

--- app/src/main/kotlin/com/ice1000/plastic/SettingsActivity.kt
+++ app/src/main/kotlin/com/ice1000/plastic/SettingsActivity.kt
@@ -1,19 +1,26 @@
 package com.ice1000.plastic
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.SeekBar
+import android.widget.TextView
 import data.constants.LAYOUT_LIST
 import data.constants.LAYOUT_PREFERENCE
+import data.constants.TEXT_SIZE
 import kotlinx.android.synthetic.main.activity_settings.*
 import utils.BaseActivity
 
 class SettingsActivity : BaseActivity() {
 
+    private val textSizeMax = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         setSupportActionBar(toolbar_settings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -32,9 +39,18 @@ class SettingsActivity : BaseActivity() {
         layoutSpinner.adapter = adapter
         layoutSpinner.onItemSelectedListener = SpinnerSelected()
 
+        val textSizeSeeker = text_size_settings_seeker
+        val textSizeShower = text_size_settings_shower
+
+        textSizeSeeker.max = textSizeMax
+        textSizeSeeker.progress = TEXT_SIZE.readInt(16)
+        textSizeSeeker.setOnSeekBarChangeListener(
+                TextSizeSeeker(textSizeMax, textSizeShower))
+
     }
 
     inner class SpinnerSelected() : AdapterView.OnItemSelectedListener {
+
         override fun onNothingSelected(p0: AdapterView<*>?) {
             LAYOUT_PREFERENCE.save(LAYOUT_LIST)
         }
@@ -50,5 +66,31 @@ class SettingsActivity : BaseActivity() {
                 p3: Long) {
             LAYOUT_PREFERENCE.save(p2)
         }
+    }
+
+    inner class TextSizeSeeker(var max: Int, var shower: TextView) :
+            SeekBar.OnSeekBarChangeListener {
+
+        private var size = 16
+
+        /**
+         * textSize must > 6
+         */
+        override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            if (p1 > 5) {
+                shower.text = "$size"
+                size = p1
+            }
+            Log.v("important", "progress = $size")
+        }
+
+        override fun onStartTrackingTouch(p0: SeekBar?) {
+            Log.v("important", "start, max = $max")
+        }
+
+        override fun onStopTrackingTouch(p0: SeekBar?) {
+            TEXT_SIZE.save(size)
+        }
+
     }
 }
